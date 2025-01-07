e02327327c12f1914d5c47bbc61654fb7a24b191
Leandro Gama <57805228+Leandro-Gama@users.noreply.github.com>
2025-01-07 10:54:15 -0300

    LC_88

---
+++ problems/array_string/java/LC_88.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int indexM = m - 1;
+        int indexN = n - 1;
+        int indexMerged = m + n - 1;
+
+        while(indexN >= 0){
+            if (indexM >= 0 && nums1[indexM] > nums2[indexN])
+                nums1[indexMerged--] = nums1[indexM--];
+            else
+                nums1[indexMerged--] = nums2[indexN--];
+        }
+    }
+}
